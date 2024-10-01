dc02545241f5f519ebb785a423f842a7be18e9b9
AntonLaoshi <anton_kostyuk@mail.ru>
2024-10-01 19:55:15 +0800

    update cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,3 @@
 import readlineSync from 'readline-sync';
-console.log('Welcome to the Brain Games!');
-export var userName = readlineSync.question('May I have your name? ');
+export var userName = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
 export var greeting = console.log('Hello, ' + userName + '!');
