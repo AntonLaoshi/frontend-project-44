7e3cc40199ea53f5a910189a4744cf518058d3f2
AntonLaoshi <anton_kostyuk@mail.ru>
2024-10-03 13:06:39 +0800

    remove bin"

--- bin/brain-games.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import { greeting } from "../src/cli.js";
-import playEvenGame from "./games/brain-even.js"
-import playCalcGame from "./games/brain-calc.js"
-import playGCDGame from "./games/brain-gcd.js";
-import playProgressionGame from "./games/brain-progression.js";
-import playPrimeGame from "./games/brain-prime.js";
