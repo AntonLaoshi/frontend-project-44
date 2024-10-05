fcc9568935a1f92c1c9f4544b466416865472f10
AntonLaoshi <anton_kostyuk@mail.ru>
2024-10-05 23:09:30 +0800

    update brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -7,9 +7,9 @@ export const greeting = console.log(`Hello, ${userName}!`);
 console.log('What is the result of the expression?');
 const operators = ['+', '-', '*'];
 const playRound = () => {
-  let number1 = getNumber20();
-  let number2 = getNumber20();
-  let selectedOperatorIndex = Math.floor(Math.random() * operators.length);
+  const number1 = getNumber20();
+  const number2 = getNumber20();
+  const selectedOperatorIndex = Math.floor(Math.random() * operators.length);
   let correctAnswer;
   if (operators[selectedOperatorIndex] === '+') {
     correctAnswer = number1 + number2;
@@ -19,12 +19,12 @@ const playRound = () => {
     correctAnswer = number1 * number2;
   }
   const userAnswer = readlineSync.question(`Question: ${number1} ${operators[selectedOperatorIndex]} ${number2} `);
-  console.log('Your answer: ' + userAnswer);
+  console.log(`Your answer: ${userAnswer}`);
   if (Number(userAnswer) === correctAnswer) {
     console.log('Correct!');
     return true;
-  } 
-  console.log("'" + userAnswer + "'" + " is wrong answer ;(. Correct answer was '" + correctAnswer + "'.\nLet's try again, " + userName + "!");
+  }
+  console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
   return false;
 };
 playGame(playRound, userName);
