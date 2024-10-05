21c16e580c9ac38c1000595c2bbe0c73a59e854e
AntonLaoshi <anton_kostyuk@mail.ru>
2024-10-05 11:57:48 +0800

    update brain-prime.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -2,46 +2,39 @@
 import readlineSync from 'readline-sync';
 import { getNumberCeil100 } from '../src/index.js';
 
-export var userName = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
-export var greeting = console.log('Hello, ' + userName + '!');
+export const userName = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
+export const greeting = console.log(`Hello, ${userName}!`);
 console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 const playRound = () => {
-    const number = getNumberCeil100();
-    const userAnswer = readlineSync.question('Question: ' + number + ' ');
-    console.log('Your answer: ' + userAnswer);
-    let correctAnswer;
-    if (number < 2) {
-        correctAnswer = 'no';
-    } else if (number === 2 || number === 3) {
-        correctAnswer = 'yes';
+  const number = getNumberCeil100();
+  const userAnswer = readlineSync.question(`Question: ${number} `);
+  console.log(`Your answer: ${userAnswer}`);
+  let correctAnswer;
+  for (let i = Math.floor(number / 2); i > 1; i -= 1) {
+    if (number === 2 || number === 3) {
+      correctAnswer = 'yes';
+      break;
+    } else if (number < 2 || number % i === 0 ) {
+      correctAnswer = 'no';
+      break;
     } else {
-        for (let i = Math.floor(number / 2); i > 1; i--) {
-            if (number === 2) {
-                correctAnswer = 'yes';
-                break;
-            } else if (number % i === 0) {
-                correctAnswer = 'no';
-                break;
-            }
-            correctAnswer = 'yes';
-        }
+      correctAnswer = 'yes'
     }
-    if (userAnswer.toLowerCase() === correctAnswer) {
-        console.log('Correct!');
-        return true;
-    } 
-    console.log("'" + userAnswer + "'" + " is wrong answer ;(. Correct answer was '" + correctAnswer + "'.\nLet's try again, " + userName + "!");
-    return false;
+  }
+  if (userAnswer.toLowerCase() === correctAnswer) {
+    console.log('Correct!');
+    return true;
+  } 
+  console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
+  return false;
 };
 const playGame = () => {
-    for (let i = 0; i < 3; i++) {
-      const isCorrect = playRound();
-      
-      if (!isCorrect) {
-        return;
-      }
+  for (let i = 0; i < 3; i += 1) {
+    const isCorrect = playRound();
+    if (!isCorrect) {
+      return;
     }
-    console.log('Congratulations, ' + userName + '!');
-  };
-  
-  playGame();
+  }
+  console.log('Congratulations, ' + userName + '!');
+};
+playGame();
