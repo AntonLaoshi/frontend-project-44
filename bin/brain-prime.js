a67b4070fd38b05b9477f692b26db66044b05c80
AntonLaoshi <anton_kostyuk@mail.ru>
2024-10-05 20:24:13 +0800

    update brain-prime.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import { getNumberCeil100 } from '../src/index.js';
+import { getNumberCeil100, playGame } from '../src/index.js';
 
 export const userName = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
 export const greeting = console.log(`Hello, ${userName}!`);
@@ -28,13 +28,4 @@ const playRound = () => {
   console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
   return false;
 };
-const playGame = () => {
-  for (let i = 0; i < 3; i += 1) {
-    const isCorrect = playRound();
-    if (!isCorrect) {
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
-};
-playGame();
+playGame(playRound, userName);
