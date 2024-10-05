979214dd1e4c89350f7f5eba6fe2b692a1f10b6e
AntonLaoshi <anton_kostyuk@mail.ru>
2024-10-05 10:52:51 +0800

    update brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
+import { getNumber20 } from '../src/index.js';
 
 export var userName = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
 export var greeting = console.log('Hello, ' + userName + '!');
 console.log('What is the result of the expression?');
-const getNumber = () => Math.round(Math.random() * 20);
 const operators = [{
     sign: '+',
     method: function(a, b){return a + b}
@@ -17,8 +17,8 @@ const operators = [{
 }
 ];
 const playRound = () => {
-    let number1 = getNumber();
-    let number2 = getNumber();
+    let number1 = getNumber20();
+    let number2 = getNumber20();
     let selectedOperatorIndex = Math.floor(Math.random() * operators.length);
     const correctAnswer = operators[selectedOperatorIndex].method(number1, number2);
     const userAnswer = readlineSync.question('Question: ' + number1 + ' ' + operators[selectedOperatorIndex].sign + ' ' + number2 + ' ');
