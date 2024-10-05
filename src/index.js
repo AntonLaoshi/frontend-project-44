c557e90ee2f5f8100a6ece1fd945e0a8ddacc4e8
AntonLaoshi <anton_kostyuk@mail.ru>
2024-10-05 10:51:49 +0800

    add index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import readlineSync from 'readline-sync';
+
+export const getNumber100 = () => Math.round(Math.random() * 100);
+export const getNumber20 = () => Math.round(Math.random() * 20);
+export const getNumberCeil100 = () => Math.ceil(Math.random() * 100);
