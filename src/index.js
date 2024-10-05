fbe8c2521b24867674c1f6080acc4165cef82ba7
AntonLaoshi <anton_kostyuk@mail.ru>
2024-10-05 20:25:08 +0800

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 export const getNumber100 = () => Math.round(Math.random() * 100);
 export const getNumber20 = () => Math.round(Math.random() * 20);
 export const getNumberCeil100 = () => Math.ceil(Math.random() * 100);
+export const playGame = (fn, name) => {
+  for (let i = 0; i < 3; i++) {
+    const isCorrect = fn();
+    if (!isCorrect) {
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+};
