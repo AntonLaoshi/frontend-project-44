4e32449d0676094e6000510523aa0869057d8623
AntonLaoshi <anton_kostyuk@mail.ru>
2024-10-05 11:58:31 +0800

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import readlineSync from 'readline-sync';
-
 export const getNumber100 = () => Math.round(Math.random() * 100);
 export const getNumber20 = () => Math.round(Math.random() * 20);
 export const getNumberCeil100 = () => Math.ceil(Math.random() * 100);
