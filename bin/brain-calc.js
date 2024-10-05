6cf527565fd518f6fd86a175b5ae7feceff4e607
AntonLaoshi <anton_kostyuk@mail.ru>
2024-10-05 21:08:16 +0800

    update brain-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -5,23 +5,20 @@ import { getNumber20, playGame } from '../src/index.js';
 export const userName = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
 export const greeting = console.log(`Hello, ${userName}!`);
 console.log('What is the result of the expression?');
-const operators = [{
-  sign: '+',
-  operation: function (a, b) { return a + b },
-}, {
-  sign: '-',
-  operation: function (a, b)  {return a - b },
-}, {
-  sign: '*',
-  operation: function (a, b) { return a * b },
-}
-];
+const operators = ['+', '-', '*'];
 const playRound = () => {
   let number1 = getNumber20();
   let number2 = getNumber20();
   let selectedOperatorIndex = Math.floor(Math.random() * operators.length);
-  const correctAnswer = operators[selectedOperatorIndex].operation(number1, number2);
-  const userAnswer = readlineSync.question('Question: ' + number1 + ' ' + operators[selectedOperatorIndex].sign + ' ' + number2 + ' ');
+  let correctAnswer;
+  if (operators[selectedOperatorIndex] === '+') {
+    correctAnswer = number1 + number2;
+  } else if (operators[selectedOperatorIndex] === '-') {
+    correctAnswer = number1 - number2;
+  } else {
+    correctAnswer = number1 * number2;
+  }
+  const userAnswer = readlineSync.question(`Question: ${number1} ${operators[selectedOperatorIndex]} ${number2} `);
   console.log('Your answer: ' + userAnswer);
   if (Number(userAnswer) === correctAnswer) {
     console.log('Correct!');
