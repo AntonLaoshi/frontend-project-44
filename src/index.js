97ae9cbe18ee4931d9ebc5ac162d0140208bbf71
AntonLaoshi <anton_kostyuk@mail.ru>
2024-10-10 19:25:08 +0800

    update index.js

--- src/index.js
+++ src/index.js
@@ -10,3 +10,12 @@ export const playGame = (fn, name) => {
   }
   console.log(`Congratulations, ${name}!`);
 };
+
+export const giveAnswer = (userAnswer, correctAnswer, userName) => {
+  if (userAnswer === correctAnswer) {
+    console.log('Correct!');
+    return true;
+  }
+  console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
+  return false;
+};
