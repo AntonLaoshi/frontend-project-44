5280c83add77c3c8f2dfeeaf246fdf9bfa55a37f
AntonLaoshi <anton_kostyuk@mail.ru>
2024-10-10 20:50:54 +0800

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 export const getNumber100 = () => Math.round(Math.random() * 100);
 export const getNumber20 = () => Math.round(Math.random() * 20);
 export const getNumberCeil100 = () => Math.ceil(Math.random() * 100);
