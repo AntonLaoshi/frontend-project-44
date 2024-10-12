0789bef30db6e563c89e0be4c696cb93084dd8af
AntonLaoshi <anton_kostyuk@mail.ru>
2024-10-12 15:01:17 +0800

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@
 import readlineSync from 'readline-sync';
 
 export const greeting = () => {
-  const userName = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ')
+  const userName = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
   console.log(`Hello, ${userName}!`);
   return userName;
 };
 
 export const getNumber = (min = 0, max = 100) => {
-  return Math.floor(Math.random() * (max - min + 1) + min) ;
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 export const playGame = (fn, name) => {
